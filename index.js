d8ddb63c4dbb359ddfbda23e07cd567f26167b10
Riley <waskerdu@gmail.com>
2026-09-19 00:45:38 -0700

    split into pages

--- index.js
+++ index.js
@@ -1,99 +1,71 @@
-const CANVAS_WIDTH = 360;
-const CANVAS_HEIGHT = 360;
-// const CANVAS_HEIGHT = 240;
-const CELL_SIZE = 6;
+let canvasWidth = 360;
+let canvasHeight = 360;
+let cellSize = 6;
 
 let ctx = null;
 let rootNode = null;
 let valueNode = null;
 let bgColorNode = null;
 let strokeColorNode = null;
+let canvas = null;
 
-// cool number 3598
-const numbers = [0, 1, 2, 3, 4, 5, 6, 36, 100];
-// const numbers = [0, 1, 2, 3, 4, 5, 6, 36, 100];
-// const ds = [4,5,3,4,2];
-// console.log(n);
-function randInt(min, max){
-    // note: makes range inclusive
-    max++;
-    const diff = max - min;
-    return Math.floor(Math.random() + diff) + min;
-}
-function rngNumber(){
-    const numDigits = randInt(3, 5);
-    const digits = [];
-    for(let idx = 0; idx < numDigits; idx++){
-        digits.push(randInt(2, 5));
-    }
-    let n = 0;
-    let pow = 1;
-    for (const element of digits) {
-        n += element * pow;
-        pow *= 6;
-    }
-    return n;
-}
-
-function main(){
-    valueNode = document.querySelector("#value");
-    valueNode.addEventListener("change", render);
-    bgColorNode = document.querySelector("#bg-color");
-    bgColorNode.addEventListener("change", render);
-    strokeColorNode = document.querySelector("#stroke-color");
-    strokeColorNode.addEventListener("change", render);
+function init(w, h){
     rootNode = document.querySelector("#root");
-    render();
-    // numbers.length = 0;
-    // for (let idx = 0; idx < 11; idx++) {
-    //     numbers.push(rngNumber());
-    // }
-    // let idx = randInt(0, 11);
-    // numbers.splice(idx, 0, 3598);
-    // renderNumbers();
-}
-function render(){
     while (rootNode.firstChild) {
         rootNode.removeChild(rootNode.firstChild);
     }
-    const canvas = document.createElement("canvas");
-    canvas.width = CANVAS_WIDTH;
-    canvas.height = CANVAS_HEIGHT;
+    canvas = document.createElement("canvas");
+    canvas.width = w;
+    canvas.height = h;
     rootNode.appendChild(canvas);
     ctx = canvas.getContext("2d");
-    ctx.strokeStyle = strokeColorNode.value;
+    ctx.strokeStyle = strokeColorNode?.value ?? "#C1AB00";
     ctx.lineWidth = 4;
-    ctx.fillStyle = bgColorNode.value;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    drawNumber(valueNode.valueAsNumber, CANVAS_WIDTH * 0.5, CANVAS_HEIGHT *  0.5);
-}
-function renderNumbers(){
-    while (rootNode.firstChild) {
-        rootNode.removeChild(rootNode.firstChild);
-    }
-    const canvas = document.createElement("canvas");
-    canvas.width = CANVAS_WIDTH * 2;
-    canvas.height = CANVAS_HEIGHT * numbers.length;
-    rootNode.appendChild(canvas);
-    ctx = canvas.getContext("2d");
+    ctx.fillStyle = bgColorNode?.value ?? "#000000";
     ctx.font = "72px serif";
-    ctx.strokeStyle = strokeColorNode.value;
-    ctx.lineWidth = 4;
-    ctx.fillStyle = bgColorNode.value;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    for(let idx = 0; idx < numbers.length; idx++){
-        ctx.strokeText(numbers[idx], CANVAS_WIDTH * 0.5, CANVAS_HEIGHT * (idx + 0.5));
-        drawNumber(numbers[idx], CANVAS_WIDTH * 1.5, CANVAS_HEIGHT * (idx + 0.5));
+}
+
+function calculator(){
+    valueNode = document.querySelector("#value");
+    valueNode.addEventListener("change", renderSingle);
+    bgColorNode = document.querySelector("#bg-color");
+    bgColorNode.addEventListener("change", renderSingle);
+    strokeColorNode = document.querySelector("#stroke-color");
+    strokeColorNode.addEventListener("change", renderSingle);
+    renderSingle();
+}
+function renderSingle(){
+    init(canvasWidth, canvasHeight);
+    drawNumber(valueNode.valueAsNumber, canvasWidth * 0.5, canvasHeight *  0.5);
+}
+function displayLegend(){
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 36, 100];
+    const values = numbers.map(n => [n,n]);
+    init(canvasWidth * 2, canvasHeight * values.length);
+    renderNumbers(values);
+}
+function displayKeys(){
+    const numbers = [ 1210, 627, 956, 6459, 95, 860, 3669, 1281, 7336, 95, 5750 ]
+    const values = numbers.map((n, idx) => [String.fromCharCode(idx + 65),n]);
+    init(canvasWidth * 2, canvasHeight * values.length);
+    renderNumbers(values);
+}
+function renderNumbers(values){
+    for(let idx = 0; idx < values.length; idx++){
+        const [text, number] = values[idx];
+        ctx.strokeText(text, canvasWidth * 0.5, canvasHeight * (idx + 0.5));
+        drawNumber(number, canvasWidth * 1.5, canvasHeight * (idx + 0.5));
         if(idx > 0){
-            drawLine(0, CANVAS_HEIGHT * idx, canvas.width, CANVAS_HEIGHT * idx);
+            drawLine(0, canvasHeight * idx, canvas.width, canvasHeight * idx);
         }
     }
-    drawLine(CANVAS_WIDTH, 0, CANVAS_WIDTH, canvas.height);
+    drawLine(canvasWidth, 0, canvasWidth, canvas.height);
 }
 function drawNumber(number, x, y, segments = 12){
-    drawCircle(x, y, CELL_SIZE * 10);
+    drawCircle(x, y, cellSize * 10);
     if(number < 0){
-        drawCircle(x, y, CELL_SIZE);
+        drawCircle(x, y, cellSize);
         number = -number;
     }
     const digits = toDigits(number);
@@ -117,28 +89,28 @@ function toDigits(number){
 function drawDigit(digit, x, y, angle){
     let xa, ya, xb, yb;
     if(digit === 0){
-        [xa,ya] = rotateVec(CELL_SIZE * 10, 0, angle);
-        [xb,yb] = rotateVec(CELL_SIZE * 6, 0, angle);
+        [xa,ya] = rotateVec(cellSize * 10, 0, angle);
+        [xb,yb] = rotateVec(cellSize * 6, 0, angle);
         drawLine(xa + x, ya + y, xb + x, yb + y);
         return;
     }
-    [xa,ya] = rotateVec(CELL_SIZE * 10, 0, angle);
-    [xb,yb] = rotateVec(CELL_SIZE * 26, 0, angle);
+    [xa,ya] = rotateVec(cellSize * 10, 0, angle);
+    [xb,yb] = rotateVec(cellSize * 26, 0, angle);
     drawLine(xa + x, ya + y, xb + x, yb + y);
     if(digit === 1) return;
-    [xa,ya] = rotateVec(CELL_SIZE * 26, -3 * CELL_SIZE, angle);
-    [xb,yb] = rotateVec(CELL_SIZE * 26, 3 * CELL_SIZE, angle);
+    [xa,ya] = rotateVec(cellSize * 26, -3 * cellSize, angle);
+    [xb,yb] = rotateVec(cellSize * 26, 3 * cellSize, angle);
     drawLine(xa + x, ya + y, xb + x, yb + y);
     if(digit === 2) return;
-    [xa,ya] = rotateVec(CELL_SIZE * 26, -4 * CELL_SIZE, angle);
-    drawCircle(xa + x, ya + y, CELL_SIZE);
+    [xa,ya] = rotateVec(cellSize * 26, -4 * cellSize, angle);
+    drawCircle(xa + x, ya + y, cellSize);
     if(digit === 3) return;
-    [xa,ya] = rotateVec(CELL_SIZE * 18, -3 * CELL_SIZE, angle);
-    [xb,yb] = rotateVec(CELL_SIZE * 18, 3 * CELL_SIZE, angle);
+    [xa,ya] = rotateVec(cellSize * 18, -3 * cellSize, angle);
+    [xb,yb] = rotateVec(cellSize * 18, 3 * cellSize, angle);
     drawLine(xa + x, ya + y, xb + x, yb + y);
     if(digit === 4) return;
-    [xa,ya] = rotateVec(CELL_SIZE * 18, -4 * CELL_SIZE, angle);
-    drawCircle(xa + x, ya + y, CELL_SIZE);
+    [xa,ya] = rotateVec(cellSize * 18, -4 * cellSize, angle);
+    drawCircle(xa + x, ya + y, cellSize);
 }
 function drawCircle(x, y, r){
     ctx.beginPath();
@@ -165,5 +137,3 @@ function drawLine(startX, startY, endX, endY){
     ctx.lineTo(endX, endY);
     ctx.stroke();
 }
-
-main();
