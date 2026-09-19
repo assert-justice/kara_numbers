6729e2a9525a9b614987f87b7b9ff8c6f967c171
Riley <waskerdu@gmail.com>
2026-09-18 23:56:00 -0700

    misc refactors

--- index.js
+++ index.js
@@ -1,34 +1,107 @@
 const CANVAS_WIDTH = 360;
 const CANVAS_HEIGHT = 360;
+// const CANVAS_HEIGHT = 240;
+const CELL_SIZE = 6;
 
 let ctx = null;
+let rootNode = null;
+let valueNode = null;
+let bgColorNode = null;
+let strokeColorNode = null;
+
+// cool number 3598
+const numbers = [0, 1, 2, 3, 4, 5, 6, 36, 100];
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 36, 100];
+// const ds = [4,5,3,4,2];
+// console.log(n);
+function randInt(min, max){
+    // note: makes range inclusive
+    max++;
+    const diff = max - min;
+    return Math.floor(Math.random() + diff) + min;
+}
+function rngNumber(){
+    const numDigits = randInt(3, 5);
+    const digits = [];
+    for(let idx = 0; idx < numDigits; idx++){
+        digits.push(randInt(2, 5));
+    }
+    let n = 0;
+    let pow = 1;
+    for (const element of digits) {
+        n += element * pow;
+        pow *= 6;
+    }
+    return n;
+}
 
 function main(){
-    const input = document.querySelector("#value");
-    input.addEventListener("change", e => drawNumber(e.target.valueAsNumber));
-    ctx = document.querySelector("#canvas").getContext("2d");
-    ctx.strokeStyle = "#C1AB00";
-    ctx.lineWidth = 4;
-    drawNumber(100);
+    valueNode = document.querySelector("#value");
+    valueNode.addEventListener("change", render);
+    bgColorNode = document.querySelector("#bg-color");
+    bgColorNode.addEventListener("change", render);
+    strokeColorNode = document.querySelector("#stroke-color");
+    strokeColorNode.addEventListener("change", render);
+    rootNode = document.querySelector("#root");
+    render();
+    // numbers.length = 0;
+    // for (let idx = 0; idx < 11; idx++) {
+    //     numbers.push(rngNumber());
+    // }
+    // let idx = randInt(0, 11);
+    // numbers.splice(idx, 0, 3598);
+    // renderNumbers();
 }
-function drawNumber(number){
-    ctx.setTransform();
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.translate(CANVAS_WIDTH * 0.5, CANVAS_HEIGHT * 0.5);
-    // ctx.clearRect(-CANVAS_WIDTH * 0.5, -CANVAS_HEIGHT * 0.5, CANVAS_WIDTH, CANVAS_HEIGHT);
-    // ctx.fillStyle = "red";
-    drawCircle(ctx, 0, 0, 60);
+function render(){
+    while (rootNode.firstChild) {
+        rootNode.removeChild(rootNode.firstChild);
+    }
+    const canvas = document.createElement("canvas");
+    canvas.width = CANVAS_WIDTH;
+    canvas.height = CANVAS_HEIGHT;
+    rootNode.appendChild(canvas);
+    ctx = canvas.getContext("2d");
+    ctx.strokeStyle = strokeColorNode.value;
+    ctx.lineWidth = 4;
+    ctx.fillStyle = bgColorNode.value;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    drawNumber(valueNode.valueAsNumber, CANVAS_WIDTH * 0.5, CANVAS_HEIGHT *  0.5);
+}
+function renderNumbers(){
+    while (rootNode.firstChild) {
+        rootNode.removeChild(rootNode.firstChild);
+    }
+    const canvas = document.createElement("canvas");
+    canvas.width = CANVAS_WIDTH * 2;
+    canvas.height = CANVAS_HEIGHT * numbers.length;
+    rootNode.appendChild(canvas);
+    ctx = canvas.getContext("2d");
+    ctx.font = "72px serif";
+    ctx.strokeStyle = strokeColorNode.value;
+    ctx.lineWidth = 4;
+    ctx.fillStyle = bgColorNode.value;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    for(let idx = 0; idx < numbers.length; idx++){
+        ctx.strokeText(numbers[idx], CANVAS_WIDTH * 0.5, CANVAS_HEIGHT * (idx + 0.5));
+        drawNumber(numbers[idx], CANVAS_WIDTH * 1.5, CANVAS_HEIGHT * (idx + 0.5));
+        if(idx > 0){
+            drawLine(0, CANVAS_HEIGHT * idx, canvas.width, CANVAS_HEIGHT * idx);
+        }
+    }
+    drawLine(CANVAS_WIDTH, 0, CANVAS_WIDTH, canvas.height);
+}
+function drawNumber(number, x, y, segments = 12){
+    drawCircle(x, y, CELL_SIZE * 10);
     if(number < 0){
-        drawCircle(ctx, 0, 0, 6);
+        drawCircle(x, y, CELL_SIZE);
         number = -number;
     }
     const digits = toDigits(number);
-    const da = Math.PI / 6;
+    const da = Math.PI * 2 / segments;
     let angle = - (digits.length - 1) / 2 * da;
-    ctx.rotate(angle);
     for (const d of digits) {
-        drawDigit(ctx, d);
-        ctx.rotate(da);
+        drawDigit(d, x, y, angle);
+        angle += da;
     }
 }
 function toDigits(number){
@@ -41,22 +114,33 @@ function toDigits(number){
     }
     return res;
 }
-function drawDigit(ctx, digit){
+function drawDigit(digit, x, y, angle){
+    let xa, ya, xb, yb;
     if(digit === 0){
-        drawLine(ctx, 60, 0, 60-6*4, 0);
+        [xa,ya] = rotateVec(CELL_SIZE * 10, 0, angle);
+        [xb,yb] = rotateVec(CELL_SIZE * 6, 0, angle);
+        drawLine(xa + x, ya + y, xb + x, yb + y);
         return;
     }
-    drawLine(ctx, 60, 0, 156, 0);
+    [xa,ya] = rotateVec(CELL_SIZE * 10, 0, angle);
+    [xb,yb] = rotateVec(CELL_SIZE * 26, 0, angle);
+    drawLine(xa + x, ya + y, xb + x, yb + y);
     if(digit === 1) return;
-    drawLine(ctx, 156, -18, 156, 18);
+    [xa,ya] = rotateVec(CELL_SIZE * 26, -3 * CELL_SIZE, angle);
+    [xb,yb] = rotateVec(CELL_SIZE * 26, 3 * CELL_SIZE, angle);
+    drawLine(xa + x, ya + y, xb + x, yb + y);
     if(digit === 2) return;
-    drawCircle(ctx, 156, -24, 6);
+    [xa,ya] = rotateVec(CELL_SIZE * 26, -4 * CELL_SIZE, angle);
+    drawCircle(xa + x, ya + y, CELL_SIZE);
     if(digit === 3) return;
-    drawLine(ctx, 108, -18, 108, 18);
+    [xa,ya] = rotateVec(CELL_SIZE * 18, -3 * CELL_SIZE, angle);
+    [xb,yb] = rotateVec(CELL_SIZE * 18, 3 * CELL_SIZE, angle);
+    drawLine(xa + x, ya + y, xb + x, yb + y);
     if(digit === 4) return;
-    drawCircle(ctx, 108, -24, 6);
+    [xa,ya] = rotateVec(CELL_SIZE * 18, -4 * CELL_SIZE, angle);
+    drawCircle(xa + x, ya + y, CELL_SIZE);
 }
-function drawCircle(ctx, x, y, r){
+function drawCircle(x, y, r){
     ctx.beginPath();
     ctx.moveTo(x + r, y);
     const segments = 48;
@@ -68,7 +152,14 @@ function drawCircle(ctx, x, y, r){
     }
     ctx.stroke();
 }
-function drawLine(ctx, startX, startY, endX, endY){
+function rotateVec(x, y, angle){
+    const s = Math.sin(angle);
+    const c = Math.cos(angle);
+    const nx = x * c - y * s;
+    const ny = x * s + y * c;
+    return [nx, ny];
+}
+function drawLine(startX, startY, endX, endY){
     ctx.beginPath();
     ctx.moveTo(startX, startY);
     ctx.lineTo(endX, endY);
